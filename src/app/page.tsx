8846d59b9ccd078ecc965b82d017b02911033451
Borngod <aboa.boni360@gmail.com>
2024-07-29 21:29:42 +0000

    first pantry

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div>
+    <p className="text-center font-extrabold text-4xl mt-7">Pantry Tracker</p>
+   </div>
+  );
+}
